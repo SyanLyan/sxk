ad89953d899415e16dfdfef3c854547e8533a997
SyanLyan <syanlyan1345@gmail.com>
2026-02-05 00:28:16 +0630

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repoName = "SaixKuu";
-
 const nextConfig: NextConfig = {
   reactCompiler: true,
-  output: "export",
-  trailingSlash: true,
-  basePath: isProd ? `/${repoName}` : "",
-  assetPrefix: isProd ? `/${repoName}/` : "",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? `/${repoName}` : "",
-  },
 };
 
 export default nextConfig;
